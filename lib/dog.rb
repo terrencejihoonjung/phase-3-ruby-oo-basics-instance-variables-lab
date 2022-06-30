dba9dda1bf3e7ea7eb9e54f0cc78511309f5e148
--replace-all <terrencejung928@gmail.com>
2022-06-30 10:25:44 -0700

    commit tree e9175bfcf6379fef79da57bddd7767386c7b9ab8

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
 class Dog
-
+    def name=(dog_name)
+        @this_dogs_name = dog_name
+      end
+    
+      def name
+        @this_dogs_name
+      end
 end
+
+
+odie = Dog.new
+odie.name = "Terry"
+
+momo = Dog.new
+momo.name = "Momo"
+
+puts odie.name
+puts momo.name
